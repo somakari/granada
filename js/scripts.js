5f19e5d22a54f82f15fe742ea7b79a1cd79116f4
somval <evalverde83@gmail.com>
2019-01-07 15:40:34 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,2 @@
-// Set the map variable
-const myMap = L.map('map');
-
-// Load the basemap
-const myBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
-
-// Add basemap to map id
-myBasemap.addTo(myMap);
-
 // Set view of the map
-myMap.setView([41.939948, -87.650673], 12);
+var mymap = L.map('map').setView([41.939948, -87.650673], 12);
