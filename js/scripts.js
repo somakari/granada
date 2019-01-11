8be344ae39899a918944005cc41e94bbe219f1d2
somval <evalverde83@gmail.com>
2019-01-11 19:26:48 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,36 @@
-// Define map
+// Set the map variable
 const myMap = L.map('map');
-
-// Define icon
-const coffeeCup = L.icon({
-  iconUrl: 'http://somval.github.io/coffee/images/coffee.png',
-  shadowUrl: '',
-  iconSize: [35, 65]
-});
 
 // Load the basemap
 const myBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19
+  maxZoom: 19,
+  attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
 // Add basemap to map id
 myBasemap.addTo(myMap);
 
-// Set center of the map
-myMap.setView([37.176573, -3.599046], 12);
+// Set view of the map
+myMap.setView([41.939948, -87.650673], 12);
 
 // Make an XMLHttpRequest to the JSON data
 const request = new XMLHttpRequest();
-request.open('GET', 'http://somval.github.io/coffee/js/map.json', true);
 
-request.onload = function () {
-  // begin accessing JSON data here
-  const data = JSON.parse(this.response);
+request.open('GET', 'map.json', true);
+request.onload = function() {
+    // Begin accessing JSON data here
+    const data = JSON.parse(this.response);
 
-  // Reduce neighborhoods down to how many they are, and count them
-  const neighborhoodCount = data.cafes.reduce((sums, cafe) => {
-    sums[cafe.neighborhood] = (sums[cafe.neighborhood] || 0) + 1;
-    return sums;
-  }, {});
-
-  // Create a sidebar
-  const sidebar = document.getElementById('neighborhoods');
-  const h3 = document.createElement("h3");
-  h3.innerHTML = "Neighborhood Count";
-  sidebar.appendChild(h3);
-
-  // Print all neighborhoods in sidebar
-  for (let neighborhood in neighborhoodCount) {
-    const p = document.createElement("p");
-    p.innerHTML = `<b>${neighborhood}</b> : ${neighborhoodCount[neighborhood]}`;
-    sidebar.appendChild(p);
-  }
-
-  // Print cafe markers
-  const cafes = data.cafes.map(cafe => {
-    console.log(cafe.name);
-
-    L.marker([cafe.lat, cafe.long], {
-      icon: coffeeCup
-    }).bindPopup(`
+    // Print cafe markers
+    const cafes = data.cafes.map(cafe => {
+    L.marker([cafe.lat, cafe.long]).bindPopup(`
         <h2>${cafe.name}</h2>
         <p><b>Neighborhood:</b> ${cafe.neighborhood}</p>
         <p><b>Ambiance:</b> ${cafe.ambiance}</p>
         <p><b>Flavor:</b> ${cafe.flavor}</p>
         <p><b>Comments:</b> ${cafe.comments}</p>
     `).openPopup().addTo(myMap);
-  });
+    });
 }
 
 request.send();
